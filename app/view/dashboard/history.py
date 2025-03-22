cf056815c4b36f939c141e46f61596bffcb2d6b2
fernando.figueiredo <fernando2015fraga@gmail.com>
2025-03-22 19:22:35 -0300

    [Add] Dashboard Histórico

--- app/view/dashboard/history.py
+++ app/view/dashboard/history.py
@@ -6,6 +6,6 @@ from app.controller import lancamento_controller
 
 
 def show():
-    st.header('Visão Geral', divider='gray')
+    st.header('Custos por Categoria', divider='gray')
     df_lancamento_futuro = lancamento_controller.select_categorias_por_mes()
     st.bar_chart(df_lancamento_futuro, y="valor", x="mes", color="tipo", stack=False)
